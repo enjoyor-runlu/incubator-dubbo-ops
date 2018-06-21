0cc8f7818b6c4c16dcabb26ba99b7ec900f01439
Jiakun Xu <jiakunxu@gmail.com>
2018-06-21 17:14:53 +0800

    abc

--- dubbo-monitor-simple/src/main/java/com/enjoyor/dubbo/monitor/simple/util/JDBCUtil.java
+++ dubbo-monitor-simple/src/main/java/com/enjoyor/dubbo/monitor/simple/util/JDBCUtil.java
@@ -1,70 +1,45 @@
 package com.enjoyor.dubbo.monitor.simple.util;
 
-import java.sql.Connection;
 import java.sql.SQLException;
-import java.sql.Statement;
+import java.util.List;
 import java.util.Properties;
 
 import javax.sql.DataSource;
 
-import com.alibaba.druid.pool.DruidDataSource;
+import com.alibaba.druid.pool.DruidDataSourceFactory;
+import com.alibaba.druid.util.JdbcUtils;
 import com.alibaba.dubbo.common.logger.Logger;
 import com.alibaba.dubbo.common.logger.LoggerFactory;
 
 /**
  * 
  * @author Tempest
- *
+ * 
  */
 public final class JDBCUtil {
 
 	private static final Logger logger = LoggerFactory.getLogger(JDBCUtil.class);
 
-	public static final String drvierName;
-
-	public static final String url;
-
-	public static final String userName;
-
-	public static final String password;
+	private static DataSource dataSource;
 
 	static {
 		// 读取资源文件
 		Properties proobj = PropertiesUtil.loadProperties("conf/env.properties");
-		drvierName = (String) proobj.get("db.driverName");
-		url = (String) proobj.get("db.url");
-		userName = (String) proobj.get("db.userName");
-		password = (String) proobj.get("db.password");
-	}
 
-	public static DataSource getDataSource() {
-		DruidDataSource dataSource = new DruidDataSource();
-		dataSource.setDriverClassName(drvierName);
-		dataSource.setUrl(url);
-		dataSource.setUsername(userName);
-		dataSource.setPassword(password);
-
-		return dataSource;
-	}
-
-	public static void executeUpdate(String sql) {
-		DataSource dataSource = getDataSource();
-
-		// 创建数据库连接
-		Connection conn = null;
 		try {
-			conn = dataSource.getConnection();
-			Statement stmt = conn.createStatement();// 返回的是一个数据库操作对象（网络通信的输入输出流）
-			stmt.executeUpdate(sql);// 执行数据库操作
+			dataSource = DruidDataSourceFactory.createDataSource(proobj);
 		} catch (Exception e) {
 			logger.error(e);
-		} finally {
-			// 关闭连接
-			try {
-				conn.close();
-			} catch (SQLException e) {
-				logger.error(e);
-			}
 		}
+	}
+
+	public static int executeUpdate(String sql, List<Object> parameters) {
+		try {
+			return JdbcUtils.executeUpdate(dataSource, sql, parameters);
+		} catch (SQLException e) {
+			logger.error(e);
+		}
+
+		return 0;
 	}
 }
